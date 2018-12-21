c64da05b8d56f459ba1c76c6178b2ff03c4c4991
asagal1989 <akash.sangal1989@gmail.com>
2018-12-21 17:52:41 +0530

    new changes
    new chages

--- src/suit/mainclasstest.java
+++ src/suit/mainclasstest.java
@@ -10,13 +10,14 @@ public class mainclasstest {
 
 	public static void main(String[] args) {
 
-		global_utility.Mailer mailing=new global_utility.Mailer();
+		
 		TestNG runner=new TestNG();
 		ArrayList<String> suitefiles=new ArrayList<String>();
 		File SourcePath=new File(System.getProperty("user.dir"));
 		suitefiles.add(SourcePath.getAbsolutePath()+"/testng.xml");
 		runner.setTestSuites(suitefiles);
 		runner.run();
+		global_utility.Mailer mailing=new global_utility.Mailer();
 		mailing.MailtoQC();
 	}
 
